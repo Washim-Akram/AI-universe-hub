0b7ec2342ea431e4dc8ff2516f6f64295b69e7bf
Washim-Akram <muhammadwashimakram@gamil.com>
2023-03-03 02:13:22 +0600

    Add details button and calendar icon using font awesome icon library

--- js/index.js
+++ js/index.js
@@ -14,22 +14,30 @@ const showData = (arrOfData) => {
         const div = document.createElement("div");
         div.classList.add("col");
             div.innerHTML = `
-                <div class="card h-100">
+                <div class="card h-100 p-4">
                     <img src="${data.image}" class="card-img-top" alt="${data.name}">
 
                     <div class="card-body">
-                        <h5 class="card-title">Features</h5>
-                        <p class="card-text">1.${data.features[0]}</p>
-                        <p class="card-text">1.${data.features[1]}</p>
-                        <p class="card-text">1.${data.features[2]}</p>
+                        <h4 class="card-title">Features</h4>
+                        <p class="card-text mb-1 text-secondary-emphasis
+                        ">1. ${data.features[0]}</p>
+                        <p class="card-text mb-1 text-secondary-emphasis">2. ${data.features[1]}</p>
+                        <p class="card-text mb-2 text-secondary-emphasis">3. ${data.features[2]}</p>
                     </div>
 
-                    <div class="card-footer">
-                        <h5 class="card-title">${data.name}</h5>
-                        <small
-                         class="text-muted">
-                         ${data.published_in}
-                         </small>
+                    <div class="card-footer d-flex align-items-center justify-content-between bg-white pt-4">
+                        <div>
+                            <h4 class="card-title">${data.name}</h4>
+                            <small class="text-muted fs-5 fw-medium me-2"><i class="far fa-calendar-alt"></i></small>
+                            <small class="text-muted fs-5 fw-medium">${data.published_in}</small>
+                        </div>
+                        <button
+                            type="button"
+                            class="arrow-right"
+                            >
+                            <i class="fas fa-arrow-right"></i>
+                        </button>
+
                     </div>
 
                 </div>
